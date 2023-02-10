98fa757b4bb794772ed95a952c6f71888d6eab4e
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-02-10 12:12:08 +0100

    build clean up temporary fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXXXXXX', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
