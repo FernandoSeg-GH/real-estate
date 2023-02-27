b6fe4439c5a3fbd6b226ce23b7aa9a206f40db62
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-02-27 13:53:29 +0100

    hubspot chat code

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Router } from "next/router";
 import { SSRProvider } from "react-bootstrap";
 import 'react-loading-skeleton/dist/skeleton.css'
 import "../assets/sass/main.scss";
+import Hubspot from './hubspot';
 
 export default function App({ Component, pageProps }) {
   Router.events.on("routeChangeStart", (url) => {
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
   return <>
     <SSRProvider>
       <Component {...pageProps} />;
+      <Hubspot />
     </SSRProvider>
   </>
 }
