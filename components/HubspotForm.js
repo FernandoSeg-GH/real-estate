815115c8f869801e94d8fcd9971e96e28fb34166
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-03-01 14:57:48 -0300

    fix

---
+++ components/HubspotForm.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+const HubspotForm = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//js.hsforms.net/forms/shell.js";
+    script.async = true;
+    script.onload = () => setLoading(false);
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <script
+            charSet="utf-8"
+            type="text/javascript"
+            id="hs-script-loader"
+            async
+            defer
+            src="//js.hsforms.net/forms/v2.js"
+          ></script>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              hbspt.forms.create({
+                portalId: "YOUR_PORTAL_ID",
+                formId: "YOUR_FORM_ID",
+                onFormSubmit: function($form) {
+                  console.log("Form submitted");
+                }
+              });
+            `,
+            }}
+          />
+          <div id="hubspotForm"></div>
+        </div>
+      )}
+    </div>
+  );
+};
