1a4d67d755bd55e39c8e807929eada2952ebd645
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-03-01 16:24:36 -0300

    styling

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -8,106 +8,110 @@ import HubspotForm from "../HubspotForm";
 
 const Footer = () => {
   return (
-    <div className="footer footer-padding-t">
-      <div className="container">
-        <div className="row">
-          <div className="footer__top mx-auto customText">
-            <h3>Más de 15 años en el sector inmobiliario</h3>
-            <p className="text-white">Contáctenos para cotizar su inmueble o para conocer oportunidades en el mercado.</p>
-            {/* <MailchimpSubscribe
-              url={MAILCHAIMP}
-              render={({ subscribe, status, message }) => (
-                <SubscribeForm
-                  status={status}
-                  message={message}
-                  onValidated={(formData) => subscribe(formData)}
-                />
-              )}
-            /> */}
-          </div>
+    <>
+      <div className="row">
+        <div className="footer__top mx-auto customText">
+          <h3>Más de 15 años en el sector inmobiliario</h3>
+          <p className="text-white">Contáctenos para cotizar su inmueble o para conocer oportunidades en el mercado.</p>
+        </div>
+        <div className="hubspotFormWrapper">
           <HubspotForm  />
-        </div>
-        <div className="row footer-padding">
-          <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
-            <div className="footer__logo">
-              <h1>CanVai S.L.</h1>
-              <p style={{color:'white'}}>NIF B72506785</p>
-              <img src='/favicon.ico' width='50' height='50' style={{borderRadius:'0.5rem'}}></img>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
-            <div className="footer__content">
-              <h3>Ubicación</h3>
-              <ul>
-                <li>
-                  Av. Diagonal & c/ de Bilbao, Barcelona 08018
-                </li>
-                <li>
-                  <GoLocation />{" "}
-                  <a
-                    href="https://www.google.com/maps/place/Avinguda+Diagonal+%26+Carrer+de+Bilbao,+08018+Barcelona/@41.4061994,2.1968254,17z/data=!3m1!4b1!4m5!3m4!1s0x12a4a33be71c8e59:0x49f36ffffe315c87!8m2!3d41.4061994!4d2.1990194"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Ver en Google Maps
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-6">
-            <div className="footer__social">
-              <h3>Redes Sociales</h3>
-              <p>¡Síguenos para obtener novedades!</p>
-              <ul>
-                <li>
-                  <a
-                    href="https://www.facebook.com/canvaisl/"
-                    target="_blank"
-                    aria-label="Facebook"
-                    className="icon"
-                  >
-                    <FaFacebookF />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.instagram.com/canvaisl/"
-                    target="_blank"
-                    aria-label="Instagram"
-                    className="icon"
-                  >
-                    <FaInstagram />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    aria-label="LinkedIn"
-                    target="_blank"
-                    href="https://www.linkedin.com/company/can-vai-sl/"
-                    className="icon"
-                  >
-                    <FaLinkedin />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="footer__copyright m-20px-t m-20px-b">
-          <div className="row">
-            <div className="col-12">
-              <p className="m-0 text-center">&copy; 2023 All right reserved.</p>
-            </div>
-          </div>
+          <div className="customBlock"/>
         </div>
       </div>
-      <style>{`
-        .customText {
-          color: white;
-        }
-      `}</style>
-    </div>
+      <div className="footer footer-padding-t">
+        <div className="container">
+          <div className="row footer-padding">
+            <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
+              <div className="footer__logo">
+                <h1>CanVai S.L.</h1>
+                <p style={{color:'white'}}>NIF B72506785</p>
+                <img src='/favicon.ico' width='50' height='50' style={{borderRadius:'0.5rem'}}></img>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
+              <div className="footer__content">
+                <h3>Ubicación</h3>
+                <ul>
+                  <li>
+                    Av. Diagonal & c/ de Bilbao, Barcelona 08018
+                  </li>
+                  <li>
+                    <GoLocation />{" "}
+                    <a
+                      href="https://www.google.com/maps/place/Avinguda+Diagonal+%26+Carrer+de+Bilbao,+08018+Barcelona/@41.4061994,2.1968254,17z/data=!3m1!4b1!4m5!3m4!1s0x12a4a33be71c8e59:0x49f36ffffe315c87!8m2!3d41.4061994!4d2.1990194"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Ver en Google Maps
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="footer__social">
+                <h3>Redes Sociales</h3>
+                <p>¡Síguenos para obtener novedades!</p>
+                <ul>
+                  <li>
+                    <a
+                      href="https://www.facebook.com/canvaisl/"
+                      target="_blank"
+                      aria-label="Facebook"
+                      className="icon"
+                    >
+                      <FaFacebookF />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/canvaisl/"
+                      target="_blank"
+                      aria-label="Instagram"
+                      className="icon"
+                    >
+                      <FaInstagram />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      aria-label="LinkedIn"
+                      target="_blank"
+                      href="https://www.linkedin.com/company/can-vai-sl/"
+                      className="icon"
+                    >
+                      <FaLinkedin />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer__copyright m-20px-t m-20px-b">
+            <div className="row">
+              <div className="col-12">
+                <p className="m-0 text-center">&copy; 2023 All right reserved.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <style>{`
+          .hubspotFormWrapper {
+            position: relative;
+          }
+          .customBlock {
+            height: 60px;
+            width: 100%;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            background: #fff;
+            z-index: 1;
+          }
+        `}</style>
+      </div>
+    </>
   )
 }
 
