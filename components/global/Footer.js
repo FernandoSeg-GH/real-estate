b648009737f990854f4e2c59beb74c71a71d9cae
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-03-01 15:45:10 -0300

    cleanup

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -21,35 +21,29 @@ const HubspotForm = () => {
 
   return (
     <div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          <script
-            type="text/javascript"
-            id="hs-script-loader"
-            async
-            defer
-            src="//js-eu1.hsforms.net/forms/embed/v2.js"
-          ></script>
-          <script
-            type="text/javascript"
-            region="eu1"
-            dangerouslySetInnerHTML={{
-              __html: `
-              hbspt.forms.create({
-                portalId: "27004540",
-                formId: "d3eebe7c-97a6-48ca-9c26-5a68604f291f",
-                onFormSubmit: function($form) {
-                  console.log("Form submitted");
-                }
-              });
-            `,
-            }}
-          />
-          <div id="hubspotForm"></div>
-        </div>
-      )}
+      <script
+        type="text/javascript"
+        id="hs-script-loader"
+        async
+        defer
+        src="//js-eu1.hsforms.net/forms/embed/v2.js"
+      ></script>
+      <script
+        type="text/javascript"
+        region="eu1"
+        dangerouslySetInnerHTML={{
+          __html: `
+          hbspt.forms.create({
+            portalId: "27004540",
+            formId: "d3eebe7c-97a6-48ca-9c26-5a68604f291f",
+            onFormSubmit: function($form) {
+              console.log("Form submitted");
+            }
+          });
+        `,
+        }}
+      />
+      <div id="hubspotForm"></div>
     </div>
   );
 };
