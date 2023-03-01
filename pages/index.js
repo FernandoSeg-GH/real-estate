eb4395c5f3d7aea5eab622dc7dce81eb83ccfd6a
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-03-01 15:58:43 -0300

    updates

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,67 @@ import PropertyListing from "../components/property-listing";
 import Testimonial from "../components/testimonial";
 import { API_URL } from "../config";
 import Hubspot from "./hubspot";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 // export default function Home({ property }) {
-//   const { data } = property;
+//   const { data } = property;  
+
+
+const HubspotForm = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = "//js.hsforms.net/forms/shell.js";
+    script.async = true;
+    script.onload = () => setLoading(false);
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+
+      ) : (
+        <div>
+          <script
+
+            type="text/javascript"
+            id="hs-script-loader"
+            async
+            defer
+            src="//js-eu1.hsforms.net/forms/embed/v2.js"
+          ></script>
+          <script
+
+
+            type="text/javascript"
+
+            region="eu1"
+
+            dangerouslySetInnerHTML={{
+              __html: `
+              hbspt.forms.create({
+                portalId: "27004540",
+                formId: "d3eebe7c-97a6-48ca-9c26-5a68604f291f",
+                onFormSubmit: function($form) {
+                  console.log("Form submitted");
+                }
+              });
+            `,
+            }}
+          />
+          <div id="hubspotForm"></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default function Home({ }) {
 
   return (
@@ -21,6 +77,8 @@ export default function Home({ }) {
       {/* <PropertyListing data={data} /> */}
       <Testimonial />
       <Hubspot />
+      <HubspotFor /> 
+      <p>Test</p>
     </Layout>
   );
 }
