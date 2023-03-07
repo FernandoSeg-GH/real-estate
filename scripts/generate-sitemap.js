eaace5365c518bb7dfb0de9d9f49682d5a121ec2
Lorenzo Pasqualini <63103853+lorenzopasqualini@users.noreply.github.com>
2023-03-07 16:17:03 +0100

    update

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -2,7 +2,7 @@ const sitemap= require('nextjs-sitemap-generator');
 const path= require('path');
 
 sitemap({
-    baseUrl: 'https://can-vai.com/',
+    baseUrl: 'https://can-vai.com',
     pagesDirectory: path.resolve(__dirname,'../pages/'),
     targetDirectory: path.resolve(__dirname, '../pages/'),
     ignoredExtensions: ['images'],
