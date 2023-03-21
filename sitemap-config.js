9a8e4b54f825291236b5348a3493b5f2dbd58023
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-03-21 11:47:22 -0300

    script for prod

--- sitemap-config.js
+++ sitemap-config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: ['https://can-vai.com', 'https://www.can-vai.com', 'http://localhost:3000'],
+  siteUrl: 'https://can-vai.com, https://www.can-vai-com, http://www.can-vai-com, http://www.can-vai-com, http://localhost:3000',
   generateRobotsTxt: true,
 };
